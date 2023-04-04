c5b724dd49115f730f44958448b46613e364ea17
arna <alireza.real98@gmail.com>
2023-04-04 13:22:57 +0330

    implement data generator class

---
+++ lib/DataGenerator.dart
@@ -0,0 +1,104 @@
+import 'package:travelapp/datamodel/TravelItem.dart';
+
+class DataGenerator{
+  static List<TravelItem> getTravelItems(){
+    List<TravelItem> travelItems = [];
+
+    TravelItem travelItem = TravelItem();
+    travelItem.setImage = "assets/images/sky";
+    travelItem.setTitle = "sky";
+    travelItem.setLocation = "Lorem, ipsum 01";
+    travelItem.setDistance = 47;
+    travelItem.setTemp = 39;
+    travelItem.setRating = 5;
+    travelItem.setDescription = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis eleifend quam adipiscing vitae proin sagittis. Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Sed faucibus turpis in eu. Lobortis feugiat vivamus at augue eget. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Nec feugiat nisl pretium fusce id velit ut. A iaculis at erat pellentesque adipiscing. Sit amet purus gravida quis blandit turpis cursus in. Nisl tincidunt eget nullam non nisi.
+
+    Et odio pellentesque diam volutpat commodo. Ac auctor augue mauris augue. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Amet justo donec enim diam vulputate ut pharetra sit. Aliquam malesuada bibendum arcu vitae elementum. Nibh mauris cursus mattis molestie a iaculis at erat. Diam donec adipiscing tristique risus nec feugiat in. Orci dapibus ultrices in iaculis nunc sed. Et netus et malesuada fames ac turpis egestas. Tristique senectus et netus et malesuada fames ac turpis. Pretium nibh ipsum consequat nisl vel pretium lectus. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Egestas pretium aenean pharetra magna. At in tellus integer feugiat.
+
+    Vitae auctor eu augue ut lectus arcu bibendum at. Nullam non nisi est sit amet facilisis magna etiam tempor. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Suspendisse faucibus interdum posuere lorem ipsum. Ut lectus arcu bibendum at varius vel. Consectetur lorem donec massa sapien faucibus et molestie. Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Nisl purus in mollis nunc sed id. Nam at lectus urna duis convallis. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Vel fringilla est ullamcorper eget nulla facilisi etiam. Sapien nec sagittis aliquam malesuada bibendum. Magna eget est lorem ipsum dolor sit amet consectetur. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Sed adipiscing diam donec adipiscing tristique. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
+
+    Aenean sed adipiscing diam donec. Ut sem nulla pharetra diam. Penatibus et magnis dis parturient. Ornare suspendisse sed nisi lacus. Facilisi nullam vehicula ipsum a arcu cursus vitae. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Accumsan in nisl nisi scelerisque eu ultrices. Aliquet eget sit amet tellus cras adipiscing enim eu. Gravida dictum fusce ut placerat orci. Odio facilisis mauris sit amet massa. Sodales neque sodales ut etiam sit amet nisl purus. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Nisl suscipit adipiscing bibendum est ultricies. Egestas congue quisque egestas diam in arcu cursus. Malesuada pellentesque elit eget gravida cum sociis. Lacus vestibulum sed arcu non. Hac habitasse platea dictumst vestibulum. Montes nascetur ridiculus mus mauris vitae ultricies leo integer. Morbi tristique senectus et netus et malesuada fames.
+    """;
+    travelItem.setPrice = 12324;
+    travelItems.add(travelItem);
+
+    travelItem = TravelItem();
+    travelItem.setImage = "assets/images/bird";
+    travelItem.setTitle = "bird";
+    travelItem.setLocation = "Lorem, ipsum 02";
+    travelItem.setDistance = 66;
+    travelItem.setTemp = -4;
+    travelItem.setRating = 1.5;
+    travelItem.setDescription = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis eleifend quam adipiscing vitae proin sagittis. Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Sed faucibus turpis in eu. Lobortis feugiat vivamus at augue eget. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Nec feugiat nisl pretium fusce id velit ut. A iaculis at erat pellentesque adipiscing. Sit amet purus gravida quis blandit turpis cursus in. Nisl tincidunt eget nullam non nisi.
+
+    Et odio pellentesque diam volutpat commodo. Ac auctor augue mauris augue. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Amet justo donec enim diam vulputate ut pharetra sit. Aliquam malesuada bibendum arcu vitae elementum. Nibh mauris cursus mattis molestie a iaculis at erat. Diam donec adipiscing tristique risus nec feugiat in. Orci dapibus ultrices in iaculis nunc sed. Et netus et malesuada fames ac turpis egestas. Tristique senectus et netus et malesuada fames ac turpis. Pretium nibh ipsum consequat nisl vel pretium lectus. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Egestas pretium aenean pharetra magna. At in tellus integer feugiat.
+
+    Vitae auctor eu augue ut lectus arcu bibendum at. Nullam non nisi est sit amet facilisis magna etiam tempor. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Suspendisse faucibus interdum posuere lorem ipsum. Ut lectus arcu bibendum at varius vel. Consectetur lorem donec massa sapien faucibus et molestie. Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Nisl purus in mollis nunc sed id. Nam at lectus urna duis convallis. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Vel fringilla est ullamcorper eget nulla facilisi etiam. Sapien nec sagittis aliquam malesuada bibendum. Magna eget est lorem ipsum dolor sit amet consectetur. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Sed adipiscing diam donec adipiscing tristique. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
+
+    Aenean sed adipiscing diam donec. Ut sem nulla pharetra diam. Penatibus et magnis dis parturient. Ornare suspendisse sed nisi lacus. Facilisi nullam vehicula ipsum a arcu cursus vitae. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Accumsan in nisl nisi scelerisque eu ultrices. Aliquet eget sit amet tellus cras adipiscing enim eu. Gravida dictum fusce ut placerat orci. Odio facilisis mauris sit amet massa. Sodales neque sodales ut etiam sit amet nisl purus. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Nisl suscipit adipiscing bibendum est ultricies. Egestas congue quisque egestas diam in arcu cursus. Malesuada pellentesque elit eget gravida cum sociis. Lacus vestibulum sed arcu non. Hac habitasse platea dictumst vestibulum. Montes nascetur ridiculus mus mauris vitae ultricies leo integer. Morbi tristique senectus et netus et malesuada fames.
+    """;
+    travelItem.setPrice = 12324;
+    travelItems.add(travelItem);
+    
+    travelItem = TravelItem();
+    travelItem.setImage = "assets/images/space";
+    travelItem.setTitle = "spcace";
+    travelItem.setLocation = "Lorem, ipsum 03";
+    travelItem.setDistance = 234;
+    travelItem.setTemp = 4;
+    travelItem.setRating = 4;
+    travelItem.setDescription = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis eleifend quam adipiscing vitae proin sagittis. Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Sed faucibus turpis in eu. Lobortis feugiat vivamus at augue eget. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Nec feugiat nisl pretium fusce id velit ut. A iaculis at erat pellentesque adipiscing. Sit amet purus gravida quis blandit turpis cursus in. Nisl tincidunt eget nullam non nisi.
+
+    Et odio pellentesque diam volutpat commodo. Ac auctor augue mauris augue. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Amet justo donec enim diam vulputate ut pharetra sit. Aliquam malesuada bibendum arcu vitae elementum. Nibh mauris cursus mattis molestie a iaculis at erat. Diam donec adipiscing tristique risus nec feugiat in. Orci dapibus ultrices in iaculis nunc sed. Et netus et malesuada fames ac turpis egestas. Tristique senectus et netus et malesuada fames ac turpis. Pretium nibh ipsum consequat nisl vel pretium lectus. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Egestas pretium aenean pharetra magna. At in tellus integer feugiat.
+
+    Vitae auctor eu augue ut lectus arcu bibendum at. Nullam non nisi est sit amet facilisis magna etiam tempor. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Suspendisse faucibus interdum posuere lorem ipsum. Ut lectus arcu bibendum at varius vel. Consectetur lorem donec massa sapien faucibus et molestie. Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Nisl purus in mollis nunc sed id. Nam at lectus urna duis convallis. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Vel fringilla est ullamcorper eget nulla facilisi etiam. Sapien nec sagittis aliquam malesuada bibendum. Magna eget est lorem ipsum dolor sit amet consectetur. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Sed adipiscing diam donec adipiscing tristique. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
+
+    Aenean sed adipiscing diam donec. Ut sem nulla pharetra diam. Penatibus et magnis dis parturient. Ornare suspendisse sed nisi lacus. Facilisi nullam vehicula ipsum a arcu cursus vitae. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Accumsan in nisl nisi scelerisque eu ultrices. Aliquet eget sit amet tellus cras adipiscing enim eu. Gravida dictum fusce ut placerat orci. Odio facilisis mauris sit amet massa. Sodales neque sodales ut etiam sit amet nisl purus. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Nisl suscipit adipiscing bibendum est ultricies. Egestas congue quisque egestas diam in arcu cursus. Malesuada pellentesque elit eget gravida cum sociis. Lacus vestibulum sed arcu non. Hac habitasse platea dictumst vestibulum. Montes nascetur ridiculus mus mauris vitae ultricies leo integer. Morbi tristique senectus et netus et malesuada fames.
+    """;
+    travelItem.setPrice = 12324;
+    travelItems.add(travelItem);
+
+    travelItem = TravelItem();
+    travelItem.setImage = "assets/images/tiger";
+    travelItem.setTitle = "tiger";
+    travelItem.setLocation = "Lorem, ipsum 04";
+    travelItem.setDistance = 634;
+    travelItem.setTemp = 24;
+    travelItem.setRating = 2.5;
+    travelItem.setDescription = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis eleifend quam adipiscing vitae proin sagittis. Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Sed faucibus turpis in eu. Lobortis feugiat vivamus at augue eget. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Nec feugiat nisl pretium fusce id velit ut. A iaculis at erat pellentesque adipiscing. Sit amet purus gravida quis blandit turpis cursus in. Nisl tincidunt eget nullam non nisi.
+
+    Et odio pellentesque diam volutpat commodo. Ac auctor augue mauris augue. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Amet justo donec enim diam vulputate ut pharetra sit. Aliquam malesuada bibendum arcu vitae elementum. Nibh mauris cursus mattis molestie a iaculis at erat. Diam donec adipiscing tristique risus nec feugiat in. Orci dapibus ultrices in iaculis nunc sed. Et netus et malesuada fames ac turpis egestas. Tristique senectus et netus et malesuada fames ac turpis. Pretium nibh ipsum consequat nisl vel pretium lectus. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Egestas pretium aenean pharetra magna. At in tellus integer feugiat.
+
+    Vitae auctor eu augue ut lectus arcu bibendum at. Nullam non nisi est sit amet facilisis magna etiam tempor. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Suspendisse faucibus interdum posuere lorem ipsum. Ut lectus arcu bibendum at varius vel. Consectetur lorem donec massa sapien faucibus et molestie. Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Nisl purus in mollis nunc sed id. Nam at lectus urna duis convallis. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Vel fringilla est ullamcorper eget nulla facilisi etiam. Sapien nec sagittis aliquam malesuada bibendum. Magna eget est lorem ipsum dolor sit amet consectetur. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Sed adipiscing diam donec adipiscing tristique. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
+
+    Aenean sed adipiscing diam donec. Ut sem nulla pharetra diam. Penatibus et magnis dis parturient. Ornare suspendisse sed nisi lacus. Facilisi nullam vehicula ipsum a arcu cursus vitae. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Accumsan in nisl nisi scelerisque eu ultrices. Aliquet eget sit amet tellus cras adipiscing enim eu. Gravida dictum fusce ut placerat orci. Odio facilisis mauris sit amet massa. Sodales neque sodales ut etiam sit amet nisl purus. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Nisl suscipit adipiscing bibendum est ultricies. Egestas congue quisque egestas diam in arcu cursus. Malesuada pellentesque elit eget gravida cum sociis. Lacus vestibulum sed arcu non. Hac habitasse platea dictumst vestibulum. Montes nascetur ridiculus mus mauris vitae ultricies leo integer. Morbi tristique senectus et netus et malesuada fames.
+    """;
+    travelItem.setPrice = 12324;
+    travelItems.add(travelItem);
+
+    travelItem = TravelItem();
+    travelItem.setImage = "assets/images/tree";
+    travelItem.setTitle = "tree";
+    travelItem.setLocation = "Lorem, ipsum 05";
+    travelItem.setDistance = 345;
+    travelItem.setTemp = 12;
+    travelItem.setRating = 4.5;
+    travelItem.setDescription = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis eleifend quam adipiscing vitae proin sagittis. Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Sed faucibus turpis in eu. Lobortis feugiat vivamus at augue eget. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Nec feugiat nisl pretium fusce id velit ut. A iaculis at erat pellentesque adipiscing. Sit amet purus gravida quis blandit turpis cursus in. Nisl tincidunt eget nullam non nisi.
+
+    Et odio pellentesque diam volutpat commodo. Ac auctor augue mauris augue. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Amet justo donec enim diam vulputate ut pharetra sit. Aliquam malesuada bibendum arcu vitae elementum. Nibh mauris cursus mattis molestie a iaculis at erat. Diam donec adipiscing tristique risus nec feugiat in. Orci dapibus ultrices in iaculis nunc sed. Et netus et malesuada fames ac turpis egestas. Tristique senectus et netus et malesuada fames ac turpis. Pretium nibh ipsum consequat nisl vel pretium lectus. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Egestas pretium aenean pharetra magna. At in tellus integer feugiat.
+
+    Vitae auctor eu augue ut lectus arcu bibendum at. Nullam non nisi est sit amet facilisis magna etiam tempor. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Suspendisse faucibus interdum posuere lorem ipsum. Ut lectus arcu bibendum at varius vel. Consectetur lorem donec massa sapien faucibus et molestie. Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Nisl purus in mollis nunc sed id. Nam at lectus urna duis convallis. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Vel fringilla est ullamcorper eget nulla facilisi etiam. Sapien nec sagittis aliquam malesuada bibendum. Magna eget est lorem ipsum dolor sit amet consectetur. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Sed adipiscing diam donec adipiscing tristique. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
+
+    Aenean sed adipiscing diam donec. Ut sem nulla pharetra diam. Penatibus et magnis dis parturient. Ornare suspendisse sed nisi lacus. Facilisi nullam vehicula ipsum a arcu cursus vitae. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Accumsan in nisl nisi scelerisque eu ultrices. Aliquet eget sit amet tellus cras adipiscing enim eu. Gravida dictum fusce ut placerat orci. Odio facilisis mauris sit amet massa. Sodales neque sodales ut etiam sit amet nisl purus. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Nisl suscipit adipiscing bibendum est ultricies. Egestas congue quisque egestas diam in arcu cursus. Malesuada pellentesque elit eget gravida cum sociis. Lacus vestibulum sed arcu non. Hac habitasse platea dictumst vestibulum. Montes nascetur ridiculus mus mauris vitae ultricies leo integer. Morbi tristique senectus et netus et malesuada fames.
+    """;
+    travelItem.setPrice = 12324;
+    travelItems.add(travelItem);
+
+    return travelItems;
+  }
+}
