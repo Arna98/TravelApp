cc28c7531a7d3121cbd3367c5605e2414cc40ac4
arna <alireza.real98@gmail.com>
2023-04-05 13:29:59 +0330

    Implementation of cards

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class TravelInfo extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -25,15 +24,62 @@ class TravelInfoHome extends StatelessWidget {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       body: Column(children: [
+        //part 1
         Container(
           width: double.infinity,
-          height: size.height/1.8,
+          height: size.height / 1.8,
           color: Colors.red,
         ),
+        //part 2
         Expanded(
           child: Container(
             width: double.infinity,
-            color: Colors.blue,
+            color: Colors.white,
+            child: Column(children: [
+              //design cards
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Card(
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        side: BorderSide(
+                            color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
+                    child: SizedBox(
+                        width: 87,
+                        height: 87,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [Text("Distance"), Text("13Km")],
+                        )),
+                  ),Card(
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        side: BorderSide(
+                            color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
+                    child: SizedBox(
+                        width: 87,
+                        height: 87,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [Text("Temp"), Text("19\u2103")],
+                        )),
+                  ),Card(
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        side: BorderSide(
+                            color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
+                    child: SizedBox(
+                        width: 87,
+                        height: 87,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [Text("Rating"), Text("4.8")],
+                        )),
+                  ),
+                ],
+              ),
+            ]),
           ),
         )
       ]),
