3798530127da563a0b2d214e7d4c41201c45340a
arna <alireza.real98@gmail.com>
2023-04-05 12:34:29 +0330

    a general view of the user interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,42 @@
-
+import 'package:flutter/material.dart';
 
 void main() {
+  runApp(TravelInfo());
+}
+
+class TravelInfo extends StatelessWidget {
   
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TravelInfoHome(),
+    );
+  }
+}
+
+class TravelInfoHome extends StatelessWidget {
+  const TravelInfoHome({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(children: [
+        Container(
+          width: double.infinity,
+          height: size.height/1.8,
+          color: Colors.red,
+        ),
+        Expanded(
+          child: Container(
+            width: double.infinity,
+            color: Colors.blue,
+          ),
+        )
+      ]),
+    );
+  }
 }
