2f9c8acb135f5e01d4af5d729f11af9607ed3b98
arna <alireza.real98@gmail.com>
2023-04-27 10:07:04 +0330

    convert statelessWidget to statefulWidget, leanBack Statusbar and Wrap with SafeArea widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:expandable_text/expandable_text.dart';
-import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(TravelInfo());
@@ -17,190 +17,204 @@ class TravelInfo extends StatelessWidget {
   }
 }
 
-class TravelInfoHome extends StatelessWidget {
+class TravelInfoHome extends StatefulWidget {
   const TravelInfoHome({
     super.key,
   });
 
   @override
+  State<TravelInfoHome> createState() => _TravelInfoHomeState();
+}
+
+class _TravelInfoHomeState extends State<TravelInfoHome> {
+
+  @override
+  void initState() {
+      super.initState();
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+  }
+
+  @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(children: [
-        //part 1
-        Container(
-          width: double.infinity,
-          height: size.height / 1.8,
-          color: Colors.red,
-          child: Stack(
-            children: [
-              //image view
-              Container(
-                width: double.infinity,
-                height: 385,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(85),
-                      bottomRight: Radius.circular(85)),
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/sky.jpg"),
-                        fit: BoxFit.fitHeight
-                        )
-                ),
-              ),
-              //buttons
-              Positioned(
-                top: 24,
-                right: 24,
-                left: 24,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //back button
-                    Container(
-                      width: 45,
-                      height: 45,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color.fromRGBO(255, 255, 255, 0.5)),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(CupertinoIcons.back,
-                              color: Colors.black)),
-                    ),
-                    //like buuton
-                    Container(
-                      width: 45,
-                      height: 45,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color.fromRGBO(255, 255, 255, 0.5)),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(CupertinoIcons.heart,
-                              color: Colors.black)),
-                    )
-                  ],
-                ),
-              ),
-              
-            ],
-          ),
-        ),
-        //part 2
-        Expanded(
-          child: Container(
+      body: SafeArea(
+        child: Column(children: [
+          //part 1
+          Container(
             width: double.infinity,
-            color: Colors.white,
-            child: Column(children: [
-              //design cards
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  /* The container can be used instead of the card as follows:
-                  Container(
-                    width: 87,
-                    height: 87,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color:Color.fromRGBO(0, 0, 0, 0.2)),
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                    child: const Center(child: Text("its a sample")),
-                  ), */
-                  Card(
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        side: BorderSide(
-                            color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
-                    child: SizedBox(
-                        width: 87,
-                        height: 87,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [Text("Distance"), Text("13Km")],
-                        )),
+            height: size.height / 1.8,
+            color: Colors.red,
+            child: Stack(
+              children: [
+                //image view
+                Container(
+                  width: double.infinity,
+                  height: 385,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(85),
+                        bottomRight: Radius.circular(85)),
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/sky.jpg"),
+                          fit: BoxFit.fitHeight
+                          )
                   ),
-                  Card(
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        side: BorderSide(
-                            color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
-                    child: SizedBox(
-                        width: 87,
-                        height: 87,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [Text("Temp"), Text("19\u2103")],
-                        )),
-                  ),
-                  Card(
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        side: BorderSide(
-                            color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
-                    child: SizedBox(
-                        width: 87,
-                        height: 87,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [Text("Rating"), Text("4.8")],
-                        )),
-                  ),
-                ],
-              ),
-              //design description
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 16, 30, 32),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text("Description"),
-                    SizedBox(height: 8),
-                    ExpandableText(
-                      "data  datadata data datadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadata",
-                      expandText: "Read More",
-                      collapseText: "Show Less",
-                      maxLines: 3,
-                    )
-                  ],
                 ),
-              ),
-              //design price & button
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //price
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Total Price"),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text("\$23123")
-                      ],
-                    ),
-                    //button
-                    Container(
-                      width: 85,
-                      height: 85,
-                      decoration: const BoxDecoration(
-                          color: Colors.black, shape: BoxShape.circle),
-                      child: IconButton(
-                          icon: const Icon(CupertinoIcons.arrow_right),
-                          color: Colors.white,
-                          onPressed: () {},
-                          iconSize: 35),
-                    )
-                  ],
+                //buttons
+                Positioned(
+                  top: 24,
+                  right: 24,
+                  left: 24,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //back button
+                      Container(
+                        width: 45,
+                        height: 45,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromRGBO(255, 255, 255, 0.5)),
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(CupertinoIcons.back,
+                                color: Colors.black)),
+                      ),
+                      //like buuton
+                      Container(
+                        width: 45,
+                        height: 45,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromRGBO(255, 255, 255, 0.5)),
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(CupertinoIcons.heart,
+                                color: Colors.black)),
+                      )
+                    ],
+                  ),
                 ),
-              )
-            ]),
+                
+              ],
+            ),
           ),
-        )
-      ]),
+          //part 2
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Column(children: [
+                //design cards
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    /* The container can be used instead of the card as follows:
+                    Container(
+                      width: 87,
+                      height: 87,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color:Color.fromRGBO(0, 0, 0, 0.2)),
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: const Center(child: Text("its a sample")),
+                    ), */
+                    Card(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          side: BorderSide(
+                              color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
+                      child: SizedBox(
+                          width: 87,
+                          height: 87,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [Text("Distance"), Text("13Km")],
+                          )),
+                    ),
+                    Card(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          side: BorderSide(
+                              color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
+                      child: SizedBox(
+                          width: 87,
+                          height: 87,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [Text("Temp"), Text("19\u2103")],
+                          )),
+                    ),
+                    Card(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          side: BorderSide(
+                              color: Color.fromRGBO(0, 0, 0, 0.2), width: 1)),
+                      child: SizedBox(
+                          width: 87,
+                          height: 87,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [Text("Rating"), Text("4.8")],
+                          )),
+                    ),
+                  ],
+                ),
+                //design description
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 16, 30, 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text("Description"),
+                      SizedBox(height: 8),
+                      ExpandableText(
+                        "data  datadata data datadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadata",
+                        expandText: "Read More",
+                        collapseText: "Show Less",
+                        maxLines: 3,
+                      )
+                    ],
+                  ),
+                ),
+                //design price & button
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //price
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("Total Price"),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text("\$23123")
+                        ],
+                      ),
+                      //button
+                      Container(
+                        width: 85,
+                        height: 85,
+                        decoration: const BoxDecoration(
+                            color: Colors.black, shape: BoxShape.circle),
+                        child: IconButton(
+                            icon: const Icon(CupertinoIcons.arrow_right),
+                            color: Colors.white,
+                            onPressed: () {},
+                            iconSize: 35),
+                      )
+                    ],
+                  ),
+                )
+              ]),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
